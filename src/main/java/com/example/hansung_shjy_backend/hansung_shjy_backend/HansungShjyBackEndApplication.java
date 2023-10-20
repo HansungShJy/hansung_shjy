7c45d23d45080726d62f3f96156f2c40b872713d
장선호 <sunho3456@naver.com>
2023-10-20 18:34:02 +0900

    First Commit ..

---
+++ src/main/java/com/example/hansung_shjy_backend/hansung_shjy_backend/HansungShjyBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.hansung_shjy_backend.hansung_shjy_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HansungShjyBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HansungShjyBackEndApplication.class, args);
+    }
+
+}
