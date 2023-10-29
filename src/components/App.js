4a73c51101b2bda4b1dc650509cf0312224a23b9
jiyou228 <jhjy504@naver.com>
2023-10-29 15:05:42 +0900

    아이디, 비번 찾기 UI

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Signup from "./Signup";
+import Login from "./Login";
 import InviteCouple from "./InviteCouple";
+import Findid from "./Findid";
+import Findpw from "./Findpw";
 
 function App() {
   return (
-    <div>
-      <InviteCouple />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/invitecouple" element={<InviteCouple />} />
+        <Route path="/findid" element={<Findid />} />
+        <Route path="/findpw" element={<Findpw />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Findpw.js
@@ -0,0 +1,162 @@
+import React, { useState } from "react";
+import homelogo from "../assets/homelogoset.png";
+import axios from "axios";
+
+function Findpw() {
+  const [userId, setuserId] = useState("");
+  const [userBirth, setuserBirth] = useState("");
+  const [userEmail, setuserEmail] = useState("");
+
+  const onBirthHandler = (e) => {
+    setuserBirth(e.target.value);
+  };
+
+  const onEmailHandler = (e) => {
+    setuserEmail(e.target.value);
+  };
+
+  const onIdHandler = (e) => {
+    setuserId(e.target.value);
+  };
+  const FindingPW = (e) => {
+    axios
+      .post("http://localhost:3000/findpw", {
+        id: userId,
+        email: userEmail,
+        birth: userBirth,
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  return (
+    <div>
+      <div>
+        <img
+          src={homelogo}
+          alt="homelogoset.png"
+          width="380"
+          style={{
+            marginLeft: "480px",
+            marginTop: "100px",
+            marginBottom: "20px",
+          }}
+        />
+        <br />
+        <label
+          className="find-pw"
+          htmlFor="text"
+          style={{
+            fontSize: "23px",
+            fontWeight: "bold",
+            marginLeft: "630px",
+          }}
+        >
+          비밀번호 찾기
+        </label>
+        <br />
+        <label
+          style={{
+            fontSize: "17px",
+            fontWeight: "bold",
+            marginLeft: "497px",
+          }}
+        >
+          {" "}
+          아이디{" "}
+        </label>
+        <input
+          type="text"
+          userName="userId"
+          value={userId}
+          style={{
+            width: "280px",
+            height: "40px",
+            fontSize: "15px",
+            left: "200px",
+            backgroundColor: "rgba(202, 225, 245, 0.3)",
+            borderRadius: "8px",
+            border: "none",
+            marginLeft: "13px",
+            marginTop: "30px",
+          }}
+          onChange={onIdHandler}
+        />
+        <br />
+        <label
+          style={{ fontSize: "17px", fontWeight: "bold", marginLeft: "500px" }}
+        >
+          이메일
+        </label>
+        <input
+          type="Email"
+          name="Email"
+          value={userEmail}
+          placeholder="  ex) Email@gamil.com"
+          style={{
+            width: "280px",
+            height: "40px",
+            fontSize: "15px",
+            left: "200px",
+            backgroundColor: "rgba(202, 225, 245, 0.3)",
+            borderRadius: "8px",
+            border: "none",
+            marginTop: "40px",
+            marginLeft: "15px",
+          }}
+          onChange={onEmailHandler}
+        />
+        <br />
+        <label
+          style={{ fontSize: "17px", fontWeight: "bold", marginLeft: "485px" }}
+        >
+          생년월일
+        </label>
+        <input
+          type="date"
+          userName="userBirth"
+          value={userBirth}
+          placeholder="8자리로 입력해주세요 ex)19990101"
+          style={{
+            width: "280px",
+            height: "40px",
+            fontSize: "15px",
+            left: "200px",
+            backgroundColor: "rgba(202, 225, 245, 0.3)",
+            borderRadius: "8px",
+            border: "none",
+            marginTop: "40px",
+            marginLeft: "15px",
+          }}
+          onChange={onBirthHandler}
+        />
+        <br />
+        <button
+          className="Findpw-button"
+          type="button"
+          onClick={FindingPW}
+          formAction=""
+          style={{
+            fontSize: "15px",
+            fontWeight: "bold",
+            width: "280px",
+            height: "30px",
+            backgroundColor: "rgba(175, 205, 245, 0.4)",
+            color: "000000",
+            border: "0px",
+            borderRadius: "10px",
+            marginTop: "40px",
+            marginLeft: "560px",
+          }}
+        >
+          확인
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Findpw;
