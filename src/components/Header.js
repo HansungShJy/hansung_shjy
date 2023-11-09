fa0615081b9564f4a6647a09a0aba49e12efe840
jiyou228 <jhjy504@naver.com>
2023-11-09 16:10:23 +0900

    로그아웃 완

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,29 +2,20 @@ import React, { useState } from "react";
 import Nav from "./\bNav";
 import "./Header.css";
 import { Cookies } from "react-cookie";
-import axios from "axios";
 import homelogo from "../assets/homelogoset.png";
 
 function Header() {
-  const userId = useState("");
   const cookie = new Cookies();
   const encodedNickname = cookie.get("nickname");
   cookie.get("nickname", decodeURIComponent(encodedNickname));
 
   function onClickLogout() {
-    {
-      axios
-        .get(`http://localhost:3000/logout`)
-        .then((response) => {
-          console.log(response.data);
-          document.location.href = "/";
-          //props.history.push("./");
-        })
-        .catch((error) => {
-          console.log(error);
-          alert("errrrror");
-        });
-    }
+    //cookie remove .. 하고 로그인창으로 ㄱㄱ
+    cookie.remove("id");
+    cookie.remove("user_id");
+    cookie.remove("nickname");
+
+    document.location.href = "/";
   }
 
   return (
