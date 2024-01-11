3f99f24d0d6b262c05bc791b5a476f062ee3a08d
장선호 <sunho3456@naver.com>
2024-01-11 16:32:11 +0900

    plan Detail 수정 4

--- src/main/java/com/example/hansung_shjy_backend/hansung_shjy_backend/repository/PlanDetailRepository.java
+++ src/main/java/com/example/hansung_shjy_backend/hansung_shjy_backend/repository/PlanDetailRepository.java
@@ -14,13 +14,12 @@ import java.util.List;
 public interface PlanDetailRepository extends JpaRepository<PlanDetail, Integer>, JpaSpecificationExecutor<Plan> {
 
     // Plan All
-    @Query(value = "SELECT * FROM plan_detail pd WHERE pd.planID = :planID", nativeQuery = true)
-    List<PlanDetail> findByPlanID(@Param("planID") Integer planID);
+//    @Query(value = "SELECT * FROM plan_detail pd WHERE pd.planID = :planID", nativeQuery = true)
+//    List<PlanDetail> findByPlanID(@Param("planID") Integer planID);
 
     // Plan Detail
-    @Query(value = "SELECT plan_detailid, plan_check, plan_day_number, plan_end_time, plan_location, plan_number, plan_price, plan_start_time" +
-            " FROM plan_detail pd WHERE pd.planID = :planID", nativeQuery = true)
-    List<PlanDetail> findPlanDetailsByPlanID(@Param("planID") Integer planID);
+    @Query(value = "SELECT plan_detailid, plan_check, plan_day_number, plan_end_time, plan_location, plan_number, plan_price, plan_start_time FROM plan_detail pd WHERE pd.planID = :planID", nativeQuery = true)
+    List<PlanDetail> findByPlanID(@Param("planID") Integer planID);
 
     // Plan Modify
     @Query(value = "SELECT * FROM plan_detail pd WHERE pd.planID = :planID", nativeQuery = true)
